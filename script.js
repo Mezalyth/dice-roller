fc4b1f238d46d7c21ad2ec253173e64458abf8ed
Mezalyth <86799855+Mezalyth@users.noreply.github.com>
2024-08-14 21:40:30 -0700

    Update dice icon, remove +/- buttons, add randomizer for d20

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+console.log('Hello, world!');
+
+function rollDice() {
+    const diceQuantity = document.getElementById("dice-quantity").value;
+    const diceResult = document.getElementById("dice-result");
+    const values = [];
+
+    for (let i = 0; i < diceQuantity; i++) {
+        const value = Math.floor(Math.random() * 20) + 1;
+        values.push(value);
+    }
+
+    diceResult.textContent = `Dice: ${values.join(', ')}`;
+}
